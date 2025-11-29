061762d3ab88a690f6b7778bec355195f3ecb487
Stasyao37 <kapitonov.stass@gmail.com>
2025-11-29 18:18:56 +0300

    Calendar dev. step 3

--- calendar.js
+++ calendar.js
@@ -11,4 +11,51 @@ var Cal = function(divID){
         'Сб',
         'Вс',
     ]
+    this.monts = 
+    [
+        'Январь',
+        'Февраль',
+        'Март',
+        'Апрель',
+        'Май',
+        'Июнь',
+        'Июль',
+        'Август',
+        'Сентябоь',
+        'Октябрь',
+        'Ноябрь',
+        'Декабрь',
+    ]
+
+    var d = new Date();
+    this.currMonth = d.getMonth;
+    this.currYear = d.getFullYear;
+    this.currDay - d.getDate;
+}
+
+Cal.prototype.nextMonth = function(){
+    if (this.currMonth == 11){
+        this.currMonth = 0;
+        this.currYear += 1;
+    }
+    else{
+        this.currMonth +=1;
+    }
+}
+
+Cal.prototype.prevMonth = function(){
+    if (this.currMonth == 0){
+        this.currMonth = 11;
+        this.currYear -= 1;
+    }
+    else{
+        this.currMonth -= 1;
+    }
+}
+
+Cal.prototype.showCurr = function(){
+    this.showMonth(this.currYear, this.currMonth);
+}
+Cal.prorotype.showMonth = function(y, m){
+    var d = new Date()
 }
